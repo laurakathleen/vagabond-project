b9f29a4852ba5ed0e7c2a2fdd97fe0b7f15cf2dd
denzalereese <dlr249@cornell.edu>
2016-10-06 16:18:26 -0700

    adds user show route

--- vagabond-app/app/controllers/users_controller.rb
+++ vagabond-app/app/controllers/users_controller.rb
@@ -12,6 +12,10 @@ class UsersController < ApplicationController
     redirect_to root_path
   end
 
+  def show
+    @user = User.find_by_id(params[:id])
+  end
+
   private
   def user_params
     params.require(:user).permit(:first_name, :last_name, :email, :password, :current_city)
--- vagabond-app/config/routes.rb
+++ vagabond-app/config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   get '/users', to: 'users#index', as: 'users'
   get '/users/new', to: 'users#new', as: 'new_user'
   post '/users', to: 'users#create'
+  get '/users/:id', to:'users#show', as: 'user'
 end
