9805251ba5c8c711010dbf6d3a59642396d3f114
denzalereese <dlr249@cornell.edu>
2016-10-06 15:31:50 -0700

    adds sign up form

--- vagabond-app/app/controllers/users_controller.rb
+++ vagabond-app/app/controllers/users_controller.rb
@@ -2,4 +2,8 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
+
+  def new
+    @user = User.new
+  end
 end
--- vagabond-app/config/routes.rb
+++ vagabond-app/config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   root to: 'users#index'
+
+
+  get '/users', to: 'users#index', as: 'users'
+  get '/users/new', to: 'users#new', as: 'new_user'
 end
