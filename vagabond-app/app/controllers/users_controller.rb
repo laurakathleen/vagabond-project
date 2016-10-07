017c50e75a7fc80c8763a528f71e8aa229931d03
denzalereese <dlr249@cornell.edu>
2016-10-06 17:21:12 -0700

    adds edit link and update

--- vagabond-app/app/controllers/users_controller.rb
+++ vagabond-app/app/controllers/users_controller.rb
@@ -16,6 +16,17 @@ class UsersController < ApplicationController
     @user = User.find_by_id(params[:id])
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to @user
+    end
+  end
+
   private
   def user_params
     params.require(:user).permit(:first_name, :last_name, :email, :password, :current_city)
--- vagabond-app/config/routes.rb
+++ vagabond-app/config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   get '/users/new', to: 'users#new', as: 'new_user'
   post '/users', to: 'users#create'
   get '/users/:id', to:'users#show', as: 'user'
+  get '/users/:id/edit', to: 'users#edit', as: 'edit_user'
+  patch '/users/:id', to: 'users#update'
 
   get '/login', to: 'sessions#new'
   post '/sessions', to: 'sessions#create'
