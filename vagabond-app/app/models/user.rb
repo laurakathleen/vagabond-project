bfc42199ed3a33292322f8d1596ca3f042dbaccf
samdarbonne <samdarbonne@gmail.com>
2016-10-07 11:59:14 -0700

    sets relationship between users and posts

--- vagabond-app/app/models/user.rb
+++ vagabond-app/app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
-
+  has_many :posts
   def self.confirm(params)
     @user = User.find_by({email: params[:email]})
     @user ? @user.authenticate(params[:password]) : false
