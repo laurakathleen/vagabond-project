6f851bae8124e31d61a3f38a46bb8b01149537b5
denzalereese <dlr249@cornell.edu>
2016-10-07 15:39:46 -0700

    adds new post form

--- vagabond-app/app/controllers/posts_controller.rb
+++ vagabond-app/app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 class PostsController < ApplicationController
-  def index
+  def create
+    @post = Post.create(post_params)
+    redirect_to root_path
   end
 
   def show
@@ -8,11 +10,17 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
   end
 
   def edit
   end
 
   def delete
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:user_id, :city_id, :post_title, :post_content)
   end
 end
--- vagabond-app/config/routes.rb
+++ vagabond-app/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'posts/index'
+  post '/posts', to: 'posts#create'
 
   get 'posts/show/:id', to: 'posts#show', as: 'post'
 
