1e6ba43fc533afe85b50b868377e53fac3f82dd1
samdarbonne <samdarbonne@gmail.com>
2016-10-07 13:42:38 -0700

    adds one to many relationship between city and posts

--- vagabond-app/db/migrate/20161007182202_create_posts.rb
+++ vagabond-app/db/migrate/20161007182202_create_posts.rb
@@ -1,8 +1,8 @@
 class CreatePosts < ActiveRecord::Migration[5.0]
   def change
     create_table :posts do |t|
-      t.belongs_to :user
-      t.belongs_to :city
+      t.belongs_to :user, optional: true
+      t.belongs_to :city, optional: true
       t.string :post_title
       t.text :post_content
 
