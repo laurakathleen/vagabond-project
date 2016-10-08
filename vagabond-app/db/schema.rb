30c1ac8208bbb027c56f13baacd1a17097fa3a2b
samdarbonne <samdarbonne@gmail.com>
2016-10-08 14:55:07 -0700

    adds color profile class to body tag

--- vagabond-app/db/schema.rb
+++ vagabond-app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161007201919) do
+ActiveRecord::Schema.define(version: 20161008202521) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,10 +26,12 @@ ActiveRecord::Schema.define(version: 20161007201919) do
 
   create_table "posts", force: :cascade do |t|
     t.integer  "user_id"
+    t.integer  "city_id"
     t.string   "post_title"
     t.text     "post_content"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.index ["city_id"], name: "index_posts_on_city_id", using: :btree
     t.index ["user_id"], name: "index_posts_on_user_id", using: :btree
   end
 
@@ -43,6 +45,7 @@ ActiveRecord::Schema.define(version: 20161007201919) do
     t.string   "last_name"
     t.string   "home_city"
     t.string   "favorite_city"
+    t.string   "color_profile"
   end
 
 end
