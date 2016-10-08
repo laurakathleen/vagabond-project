b6d4a09ca2d9f2d821e2ea662d88a0e1185c3732
samdarbonne <samdarbonne@gmail.com>
2016-10-08 10:25:35 -0700

    fixes merge conflicts in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,10 @@ Rails.application.routes.draw do
   get 'posts/index'
   get 'posts/show/:id', to: 'posts#show', as: 'post'
   get 'posts/new'
-<<<<<<< HEAD:config/routes.rb
-  get 'posts/edit'
-  get 'posts/delete'
-=======
 
   get 'posts/:post_id/edit', to:'posts#edit', as: 'edit_post'
   patch 'posts/show/:post_id', to: 'posts#update'
   delete 'posts/show/:post_id', to: 'posts#destroy'
->>>>>>> 39b06c2702bfd28c62d72d6e46b23dfbdc304a2e:vagabond-app/config/routes.rb
 
 
   get 'cities/index'
