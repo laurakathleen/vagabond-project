760da1168012cb130ac19aa3a56d897c10cedb49
samdarbonne <samdarbonne@gmail.com>
2016-10-09 13:30:34 -0700

    fixes checking for unlogged in user admin bug

--- vagabond-app/app/controllers/cities_controller.rb
+++ vagabond-app/app/controllers/cities_controller.rb
@@ -7,9 +7,9 @@ class CitiesController < ApplicationController
     @city = City.find_by(url_display: params[:city_url_display])
     @page = params[:page]
     if @city.posts.count > (@page.to_i * 10) + 10
-    	@posts = @city.posts.all[(0 + (10 * @page.to_i))...(10 + (10 * @page.to_i))]
+    	@posts = @city.posts.all.order(:created_at)[(0 + (10 * @page.to_i))...(10 + (10 * @page.to_i))]
     else
-    	@posts = @city.posts.all[(-10..-1)]
+    	@posts = @city.posts.all.order(:created_at)[(-10..-1)]
     end
   end
 
