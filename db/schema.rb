860fff2929cc0126b40e0f1d86e7d8b005be9991
samdarbonne <samdarbonne@gmail.com>
2016-10-11 14:31:12 -0700

    fixes javascript not loading

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20161011175940) do
-=======
 ActiveRecord::Schema.define(version: 20161011192809) do
->>>>>>> cc8a90ce9c625dbcd8673cdaca9d736ef63ca9f9
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
