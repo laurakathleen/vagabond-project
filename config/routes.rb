3f3226f0b1bc62bf7d51a77a439cd0252aedb44b
ashleyvernon <avernon526@gmail.com>
2016-10-11 14:48:00 -0700

    saves comment

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,9 @@ Rails.application.routes.draw do
   get 'posts/show/:id', to: 'comments#new'
   post 'posts/show/:id', to: 'comments#create', as: 'new_comment'
   get 'posts/show/:id', to: 'comments#show'
+  get 'posts/show/:id', to: 'comments#edit', as: 'edit_comment'
+  patch 'posts/show/:post_id', to: 'comments#update'
+  
   
 
   get 'cities', to: 'cities#index'
