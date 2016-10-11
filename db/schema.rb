6298de4295c7c47e4f3348e2f98948f1dd14ff93
laurakathleen <laurakwalley@gmail.com>
2016-10-11 14:38:45 -0700

    cleans up posts-show

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20161011175940) do
-=======
-ActiveRecord::Schema.define(version: 20161011192809) do
->>>>>>> cc8a90ce9c625dbcd8673cdaca9d736ef63ca9f9
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,7 +23,6 @@ ActiveRecord::Schema.define(version: 20161011192809) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "url_display"
-    t.string   "status"
   end
 
   create_table "comments", force: :cascade do |t|
