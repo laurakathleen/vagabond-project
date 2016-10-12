a0b4bf2139de7b1ead9502c53e34cb5d292ed0c2
denzalereese <dlr249@cornell.edu>
2016-10-11 21:29:44 -0700

    fixes form focus colors

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,6 +1,6 @@
 
 $(document).on('turbolinks:load', function(){
-       $(window).on('scroll', function() {
+      $(window).on('scroll', function() {
          if ($(this).scrollTop() > 200) {
            $('#background-img').css('opacity', 0);
            $('body').css('background-color', '#bdbdbd');
@@ -9,4 +9,8 @@ $(document).on('turbolinks:load', function(){
          }
        });
       $('.modal-trigger').leanModal();
+      $('input').focus(function() {
+        $('input').css('border-bottom', '1px solid #d32f2f');
+        $('input').css('box-shadow', '0 1px 0 0 #d32f2f');
+      });
   });
