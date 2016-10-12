b573e4591fd271e066c1512e50a16f093ce3accf
denzalereese <dlr249@cornell.edu>
2016-10-11 20:13:37 -0700

    account form ui updates

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20161011175940) do
+ActiveRecord::Schema.define(version: 20161006223908) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,14 +23,7 @@ ActiveRecord::Schema.define(version: 20161011175940) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "url_display"
-  end
-
-  create_table "comments", force: :cascade do |t|
-    t.text     "comment_content"
-    t.integer  "user_id"
-    t.integer  "post_id"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.string   "status"
   end
 
   create_table "posts", force: :cascade do |t|
